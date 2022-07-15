8249480e198f21284533d0cafe0f3ea2d0a489df
Mariana Lima <marianalima.amb@gmail.com>
2022-07-15 14:53:44 -0300

    add conceitos de tipos de dados

--- Começando-a-programar-java/variaveis/Introducao-a-variaveis/src/definindo_variaveis/Main.java
+++ Começando-a-programar-java/variaveis/Introducao-a-variaveis/src/definindo_variaveis/Main.java
@@ -8,5 +8,31 @@ public class Main {
         System.out.println(i);
         System.out.println(I);
         System.out.println(CONSTANTE_VARIAVEL);
+
+//        Tipos de variáveis
+        byte b=14;
+        short s = 15785;
+        int m=15;
+        float n = 0.25F;
+        double d =0.2654156415145415415415441454;
+        System.out.println(b );
+        System.out.println(s);
+        System.out.println(m);
+        System.out.println(n);
+        System.out.println(d);
+
+//        Textual
+        char c = 'T';
+        String str = "Bootcamp code girls";
+        
+//        Boolean
+        boolean w=true;
+
+
+        System.out.println(c);
+        System.out.println(str);
+        System.out.println(w);
     }
+
+
 }
