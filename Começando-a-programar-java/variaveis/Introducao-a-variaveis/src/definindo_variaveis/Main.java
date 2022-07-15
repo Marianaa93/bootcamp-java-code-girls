eb755865f86e089e6a6a22b11868576d56f6040c
Mariana Lima <marianalima.amb@gmail.com>
2022-07-15 11:48:54 -0300

    organiza estrutura do programa

--- Começando-a-programar-java/variaveis/Introducao-a-variaveis/src/definindo_variaveis/Main.java
+++ Começando-a-programar-java/variaveis/Introducao-a-variaveis/src/definindo_variaveis/Main.java
@@ -2,9 +2,9 @@ package definindo_variaveis;
 
 public class Main {
     public static void main(String[] args) {
-        int i=2;
-                int I=2;
-                final int CONSTANTE_VARIAVEL = 4;
+        int i = 2;
+        int I = 2;
+        final int CONSTANTE_VARIAVEL = 4;
         System.out.println(i);
         System.out.println(I);
         System.out.println(CONSTANTE_VARIAVEL);
