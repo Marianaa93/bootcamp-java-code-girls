3ce6d0ac2c8bd0cbc9e9dee9d9ed26f771b14ff2
Mariana Lima <marianalima.amb@gmail.com>
2022-07-15 16:12:58 -0300

    add operadores aritiméticos

--- Começando-a-programar-java/variaveis/Introducao-a-variaveis/src/definindo_variaveis/Main.java
+++ Começando-a-programar-java/variaveis/Introducao-a-variaveis/src/definindo_variaveis/Main.java
@@ -2,37 +2,9 @@ package definindo_variaveis;
 
 public class Main {
     public static void main(String[] args) {
-        /*int i = 2;
-        int I = 2;
-        final int CONSTANTE_VARIAVEL = 4;
-        System.out.println(i);
-        System.out.println(I);
-        System.out.println(CONSTANTE_VARIAVEL);
 
-//        Tipos de variáveis
-        byte b = 14;
-        short s = 15785;
-        int m = 15;
-        float n = 0.25F;
-        double d = 0.2654156415145415415415441454;
-        System.out.println(b);
-        System.out.println(s);
-        System.out.println(m);
-        System.out.println(n);
-        System.out.println(d);
-*/
-//        Textual
-//        char c = 'T';
-//        String str = "Bootcamp code girls";
-//
-////        Boolean
-//        boolean w = true;
-//
-//
-//        System.out.println(c);
-//        System.out.println(str);
-//        System.out.println(w);
-
+        System.out.println("Variáveis");
+        variaveis();
         System.out.println("PrePos");
         prePos();
 
@@ -44,6 +16,41 @@ public class Main {
 
         System.out.println("Precedência");
         precedencia();
+
+    }
+
+    private static void variaveis() {
+        int i = 2;
+        int I = 2;
+        final int CONSTANTE_VARIAVEL = 4;
+        System.out.println("i:" + i);
+        System.out.println("I:" + I);
+        System.out.println("Constante:" + CONSTANTE_VARIAVEL);
+
+//        Tipos de variáveis
+        byte b = 14;
+        short s = 15785;
+        int m = 15;
+        float n = 0.25F;
+        double d = 0.2654156415145415415415441454;
+        System.out.println("b:" + b);
+        System.out.println("s:" + s);
+        System.out.println("m:" + m);
+        System.out.println("n:" + n);
+        System.out.println("d:" + d);
+
+//        Textual
+        char c = 'T';
+        String str = "Bootcamp code girls";
+
+//        Boolean
+        boolean w = true;
+
+
+        System.out.println("c:" + c);
+        System.out.println("str:" + str);
+        System.out.println("w:" + w);
+
 
     }
 
@@ -117,7 +124,7 @@ public class Main {
         int b = k / --i % 3 + 1; //30/10%3+1->1
         System.out.println("B:" + b);
         int c = 2;
-        c *=i += 5; // c=2*i; i=i+5-> c=2*i; i=10+5; -> c=2*15;c=30
+        c *= i += 5; // c=2*i; i=i+5-> c=2*i; i=10+5; -> c=2*15;c=30
         System.out.println("c:" + c);
 
     }
