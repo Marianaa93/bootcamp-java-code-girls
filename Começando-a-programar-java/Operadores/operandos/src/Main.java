6906630a26f7e4fc2f2752fbd02c75a7fd8b78e3
Mariana Lima <marianalima.amb@gmail.com>
2022-07-18 13:18:41 -0300

    add operandos relacionais

---
+++ Começando-a-programar-java/Operadores/operandos/src/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+    public static void main(String[] args) {
+        int i1=10;
+        int i2=20;
+
+        float f1=4.5f;
+        float f2=3.7f;
+        char c1='x';
+        char c2='y';
+        String s1="Fulano";
+        String s2="Fulano";
+        boolean b1=true;
+        boolean b2=false;
+
+        // inteiros
+        System.out.println("INTEIROS");
+        System.out.println("i1>i2:"+ (i1>i2));
+        System.out.println("i1<i2:"+ (i1<i2));
+        System.out.println("i1==i2:"+ (i1==i2));
+        System.out.println("i1>=i2:"+ (i1>=i2));
+
+
+        //float
+        System.out.println("FLOATS");
+        System.out.println("f1>f2:"+ (f1>f2));
+        System.out.println("f1<f2:"+ (f1<f2));
+        System.out.println("f1==f2:"+ (f1==f2));
+        System.out.println("f1>=f2:"+ (f1>=f2));
+
+        System.out.println("CHARS");
+        System.out.println("c1>c2:"+ (c1>c2));
+        System.out.println("c1<c2:"+ (c1<c2));
+        System.out.println("c1==c2:"+ (c1==c2));
+        System.out.println("c1>=c2:"+ (c1>=c2));
+
+        System.out.println("BOOLEANS");
+        System.out.println("b1==b2:"+ (b1==b2));
+
+
+
+    }
+
+}
