0fb988917e6f834d06f0ec3360369f5c1e6833b0
Mariana Lima <marianalima.amb@gmail.com>
2022-07-18 22:54:04 -0300

    add arrays Reverse exercicio 1

---
+++ Começando-a-programar-java/Loops/Arrays/Exercícios/ordemInversa/ordemIversa/src/OrdemInversa.java
@@ -0,0 +1,22 @@
+import java.util.Collections;
+import java.util.Scanner;
+
+public class OrdemInversa {
+
+    /*
+Crie um vetor de 6 números inteiros
+e mostre-os na ordem inversa.
+*/
+
+    public static void main(String[] args) {
+        int[] arrays = {1,2,3,4,5,6};
+
+            System.out.print(arrays);
+                    Collections.reverseOrder();
+        }
+
+
+
+    }
+
+}
