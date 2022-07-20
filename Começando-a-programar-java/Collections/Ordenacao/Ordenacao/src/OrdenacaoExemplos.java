a9721f6ec9ce4137e0d64c55f847dfb8d90148b1
Mariana Lima <marianalima.amb@gmail.com>
2022-07-19 21:10:00 -0300

    add ordenacao exemplos

---
+++ Começando-a-programar-java/Collections/Ordenacao/Ordenacao/src/OrdenacaoExemplos.java
@@ -0,0 +1,56 @@
+/*Dadas as seguintes informações sobre meus livros favoritos e seus autores,
+crie um dicionário e ordene este dicionário:
+exibindo (Nome Autor - Nome Livro);
+Autor = Hawking, Stephen  - Livro = nome: Uma Breve História do Tempo. páginas: 256
+Autor = Duhigg, Charles - Livro = nome: O Poder do Hábito, paginas: 408
+Autor = Harari, Yuval Noah  - Livro = 21 Lições Para o Século 21, páginas: 432
+*/
+
+import java.util.*;
+
+class Livro {
+    private String nome;
+    private Integer paginas;
+
+    public Livro(String nome, Integer paginas) {
+        this.nome = nome;
+        this.paginas = paginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getPaginas() {
+        return paginas;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Livro livro = (Livro) o;
+        return nome.equals(livro.nome) && paginas.equals(livro.paginas);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, paginas);
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", paginas=" + paginas +
+                '}';
+    }
+}
+
+class ComparatorNome implements Comparator<Map.Entry<String, Livro>>{
+
+    @Override
+    public int compare(Map.Entry<String, Livro> l1, Map.Entry<String, Livro> l2) {
+        return l1.getValue().getNome().compareToIgnoreCase(l2.getValue().getNome());
+    }
+}
