74705cf7e288a4858861629e234c16e5b4a50b51
Mariana Lima <marianalima.amb@gmail.com>
2022-07-20 17:12:58 -0300

    exercicios de poo

---
+++ Dominando-Java/Porgramacao_Orientada_Objeto/OOP/src/Carro.java
@@ -0,0 +1,53 @@
+class Carro {
+
+        String cor;
+        String modelo;
+        int capacidadeTanque;
+
+        //construtor tem que estar abaixo dos atributos
+    Carro(){
+
+    }
+
+    Carro(String cor,String modelo, int capacidadeTanque){
+       this.cor = cor;
+       this.modelo=modelo;
+       this.capacidadeTanque=capacidadeTanque;
+
+    }
+
+
+
+
+    //depois vem o get e sets
+
+     String getCor() {
+        return cor;
+    }
+
+     void setCor(String cor) {
+        this.cor = cor;
+    }
+
+     String getModelo() {
+        return modelo;
+    }
+
+     void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+     int getCapacidadeTanque() {
+        return capacidadeTanque;
+    }
+
+     void setCapacidadeTanque(int capacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    // método do total para encher o tanque
+
+    double totalValortanquee(double valorCombustivel){
+         return capacidadeTanque * valorCombustivel;
+    }
+}
