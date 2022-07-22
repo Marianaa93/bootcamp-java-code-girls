e78f3822a0baec464b1ed529faeabc3585bf89ec
Mariana Lima <marianalima.amb@gmail.com>
2022-07-21 21:00:25 -0300

    Cria get e set Documento

--- Dominando-Java/PraticandoOOP/PraticandoOOP/src/poo/model/Pessoa.java
+++ Dominando-Java/PraticandoOOP/PraticandoOOP/src/poo/model/Pessoa.java
@@ -2,7 +2,12 @@ package poo.model;
 
 public class Pessoa {
 
-    public  enum TipoPessoa{FISICA, JURIDICA}
+    private static final int TAMANHO_CPF = 11;
+    private static final int TAMANHO_CNPJ = 14;
+
+
+    public enum TipoPessoa {FISICA, JURIDICA}
+
     public Integer codigo;
     public String nome;
     private String documento;
@@ -14,9 +19,29 @@ public class Pessoa {
     }
 
     public void setDocumento(String documento) {
-        if (documento == null || documento.isEmpty()){
-            throw new RuntimeException("Documento não pode ser vazio");
+        if (documento == null || documento.isEmpty()) {
+            throw new RuntimeException("Documento não pode ser nulo ou vazio");
+        }
+        if (documento.length()== TAMANHO_CPF) {
+           setDocumento(documento,tipo=TipoPessoa.FISICA);
+        } else if (documento.length() == TAMANHO_CNPJ) {
+            setDocumento(documento,tipo=TipoPessoa.JURIDICA);
+        }
+        else {
+            throw new RuntimeException("Documento inválido");
         }
         this.documento = documento;
     }
+
+    private void setDocumento(String documento, TipoPessoa tipo){
+
+    }
+
+    public TipoPessoa getTipo(){
+        return tipo;
+    }
+
+
 }
+
+
